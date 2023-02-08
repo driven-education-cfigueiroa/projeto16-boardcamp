e03e746c86805d5dd850d7f320b8542cf354787d
Caio Figueirôa <cfigueiroa@gmail.com>
2023-02-08 12:42:10 -0300

    feat: add existing game validation

--- src/controllers/games.controllers.js
+++ src/controllers/games.controllers.js
@@ -2,29 +2,31 @@ import { db } from '../database/database.connection.js';
 
 export async function insertGame(req, res) {
   const { name, image, stockTotal, pricePerDay } = req.body;
-
   try {
+    const existingGame = await db.query('SELECT * FROM games WHERE name = $1', [
+      name,
+    ]);
+    if (existingGame.rowCount > 0) {
+      return res.sendStatus(409);
+    }
     const result = await db.query(
       'INSERT INTO games (name, image, "stockTotal", "pricePerDay") VALUES ($1, $2, $3, $4)',
       [name, image, stockTotal, pricePerDay]
     );
-
     if (result.rowCount === 0) {
       return res.sendStatus(400);
     }
-
     res.sendStatus(201);
   } catch (error) {
-    console.log(error);
     res.sendStatus(500);
   }
 }
 
-export async function listGames(req, res) {
+export async function listGames(_req, res) {
   try {
     const result = await db.query('SELECT * FROM games');
     res.send(result.rows);
   } catch (error) {
-    res.status(500).send(error.message);
+    res.sendStatus(500);
   }
 }
