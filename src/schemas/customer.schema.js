c3283e52e0f8388f95ced09a7ac046c982c47f88
Caio Figueirôa <cfigueiroa@gmail.com>
2023-02-08 19:04:28 -0300

    feat: validate cpf as numeric string only

--- src/schemas/customer.schema.js
+++ src/schemas/customer.schema.js
@@ -3,6 +3,6 @@ import joi from 'joi';
 export const customerSchema = joi.object({
   name: joi.string().required(),
   phone: joi.string().required(),
-  cpf: joi.string().length(11).required(),
+  cpf: joi.string().length(11).regex(/^[0-9]+$/).required(),
   birthday: joi.date().required(),
 });
